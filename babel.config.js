7c95c814b4f0fd57e027e4d5558007ebeb502e23
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-12-10 11:19:42 -0500

    Running with default App.js

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "react-native-dotenv",
-          path: ".env",
-        },
-      ],
-    ],
-  };
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
 };
